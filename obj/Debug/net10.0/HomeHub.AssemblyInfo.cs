458fccd597bbcd02378a733d80a59f7f96c89e40
Kirucoderanger <klemu@byupathway.edu>
2026-08-13 17:45:05 +0300

    Preparing for Azure deployment

--- obj/Debug/net10.0/HomeHub.AssemblyInfo.cs
+++ obj/Debug/net10.0/HomeHub.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3b039359b9a94ccff66931986b63e0232c5ec7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07e710da5be70958d67ce34f686f316ec452a145")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
